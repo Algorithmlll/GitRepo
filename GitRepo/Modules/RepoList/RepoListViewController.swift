93004cafd4e93add029192401c6affae15181f88
Chinnawat <Chinnawat@siamsquared.com>
2017-10-29 16:46:35 +0700

    Create RepoListViewController

---
+++ GitRepo/Modules/RepoList/RepoListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RepoListViewController.swift
+//  GitRepo
+//
+//  Created by Chinnawat on 10/29/2560 BE.
+//  Copyright © 2560 I'am Kerm. All rights reserved.
+//
+
+import UIKit
+
+class RepoListViewController: UIViewController {
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
